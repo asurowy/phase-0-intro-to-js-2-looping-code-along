1d4f639fc6305418a96e288cf8dacd1f4858a9f6
asurowy <101614024+asurowy@users.noreply.github.com>
2022-07-05 12:24:05 -0400

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+  let messages = []
+  for (let i = 0; i< names.length - 0; i++){
+    messages.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+  }
+return messages;  
+}
+writeCards(names, "surprise");
+
+
+
+function countDown(numbers) {
+  let i = 10
+  while (i >= 0) {
+      console.log(i);
+      --i;
+  }
+  return numbers;
+  }
+countDown(numbers);
+
